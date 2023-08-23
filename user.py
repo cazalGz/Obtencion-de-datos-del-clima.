26688d5d839b348fab9d0f955871412cf7602926
cazalGz <giotapping@gmail.com>
2023-08-23 12:30:18 -0300

    Agregando archivo al .gitignore

--- user.py
+++
@@ -1,7 +0,0 @@
-user='root'
-password='gonza'
-host='localhost'
-database_name='DatosClima'
-
-api_key='f4a9e56fb6e1d888b8abf3242d4739ad'
-output_folder = "C:/Users/Gonza/Documents/Informatorio/proyectos personales/Clima/datos del clima"
